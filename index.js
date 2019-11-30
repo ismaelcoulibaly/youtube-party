4fe1e0a48578c606313e90d4ea3f7a33e93d0106
Ismaël Coulibaly <47092200+ismaelcoulibaly@users.noreply.github.com>
2019-11-30 03:04:08 -0500

    2e commit-- le player fonctionne bien, il faut faire fonctionner les buttons. SOcket.io est installe et fonctionne plus ou mois.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
+var app=express();
 const path = require('path')
+const http=require('http').Server(app)
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + 'index.html');
+});
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+io.on('connection',function(socket){
+  socket.on('playEvent',function(msg){
+    console.log(msg);
+  });
+  console.log('a user connected');
+});
+
